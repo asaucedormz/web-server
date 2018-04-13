15702a044b0319ae99d210aca47de71cafbe46aa
Alejandro <asaucedormz@outlook.com>
2018-04-12 23:33:44 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+let app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+// helpers
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'alejandro SAUCEDO',
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'Alejandro',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
